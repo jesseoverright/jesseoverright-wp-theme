81e503351c82f13b8d5896c4324bd080f6b9428d
Jesse Overright <jesseoverright@gmail.com>
2013-12-12 16:57:52 -0600

    Added social media icons to header and refactored

--- header.php
+++ header.php
@@ -9,9 +9,17 @@
 </head>
 <body>
 <header role="banner">
-    <h1 class="site-header"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-    <div role="navigation">
-        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-    </div>
+    <h1 class="site-header">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo('name'); ?></a>
+    </h1>
+    <ul class="social-icons">
+        <li><a href="https://github.com/jesseoverright/"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/github_alt.png" alt="jesseoverright on Github"></a></li>
+        <li><a href="https://twitter.com/jesseover"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/twitter_alt.png" alt="@jesseover on Twitter"></a></li>
+        <li><a href="http://www.linkedin.com/pub/jesse-overright/5b/362/734
+"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/linkedin.png" alt="Jesse Overright on LinkedIn"></a></li>
+    </ul>
 </header>
+<div class="navigation" role="navigation">
+    <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+</div>
 <div id="page">
