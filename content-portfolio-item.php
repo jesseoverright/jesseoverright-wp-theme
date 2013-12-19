25162bcd0b11f069b6c357f14335a34ee34f1685
Jesse Overright <jesseoverright@gmail.com>
2013-12-19 15:52:48 -0600

    Fixed issues with word wrap

--- content-portfolio-item.php
+++ content-portfolio-item.php
@@ -1,7 +1,7 @@
 <article>
     <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     <?= get_the_term_list($post->ID, 'key-features', '<ul class="key-features"><li class="header">Features:</li><li>', '</li><li>', '</li></ul>')  ?>
-    <h3 class="project-url"><a href="<?= get_post_meta($post->ID, 'project_url', true)?>"><?= get_post_meta($post->ID, 'project_url', true)?></a></h3>
+    <h3 class="project-url"><a href="<?= get_post_meta($post->ID, 'project_url', true)?>"><?= str_replace('http://','',get_post_meta($post->ID, 'project_url', true)) ?></a></h3>
     <?php the_content(); ?>
     <h4><?= get_post_meta($post->ID, 'project_date', true) ?></h4>
 </article>
