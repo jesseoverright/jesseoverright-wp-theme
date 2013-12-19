418f64ef07ddb77f9382cded4d3f407e007dc743
Jesse Overright <jesseoverright@gmail.com>
2013-12-19 11:04:02 -0600

    Moved location of post metadata

--- content-post.php
+++ content-post.php
@@ -1,7 +1,8 @@
 <article>
     <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-    <p class="post-meta">Posted on <?= get_the_date() ?></p>
     <?php the_content(); ?>
     <?= get_the_term_list($post->ID, 'category', '<ul class="categories"><li class="header">Categories:</li><li>', '</li><li>', '</li></ul>')  ?>
     <?= get_the_tag_list('<ul class="tags"><li class="header">Tags:</li><li>', '</li><li>', '</li></ul>')  ?>
+    <p class="post-meta">Posted on <?= get_the_date() ?></p>
+
 </article>
